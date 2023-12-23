123cc90d7acefebc39a4a640a7962cfa94e89186
woowooyong <woowooyong@gmail.com>
2023-12-23 21:06:34 +0800

    update: 更新ProductItem測試

--- src/components/ProductItem.test.ts
+++ src/components/ProductItem.test.ts
@@ -1,17 +1,19 @@
 import { mount } from '@vue/test-utils'
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import ProductItem from './ProductItem.vue'
 
-test('mount component', async () => {
-  expect(ProductItem).toBeTruthy()
+describe('ProductItem Component', () => {
+  test('mount component', async () => {
+    expect(ProductItem).toBeTruthy()
 
-  const wrapper = mount(ProductItem, {
-    props: {
-      title: 'Book',
-      price: 99,
-      inStock: true,
-      imageUrl: 'some/image/url'
-    }
+    const wrapper = mount(ProductItem, {
+      props: {
+        title: 'Book',
+        price: 99,
+        inStock: true,
+        imageUrl: 'some/image/url'
+      }
+    })
+    expect(wrapper.text()).toContain('Book')
   })
-  expect(wrapper.text()).toContain('Book')
 })
